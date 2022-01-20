9c05be3e6c01012f700cad8be8e4a8e995b36988
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-20 11:38:01 +0800

    replace if with switch

--- Kerusakan_Part_02/Program.cs
+++ Kerusakan_Part_02/Program.cs
@@ -19,34 +19,54 @@ namespace Kerusakan_Part_02
 
                 char key = Console.ReadKey().KeyChar;
 
-                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+                //if (key != '0' && key != '1' && key != '2' && key != '3') return; ||
+                //if (key != '0' || key != '1' || key != '2' || key != '3') return;
 
                 Console.Write("\nS: Sword, A: Arrow, anything else: Quit: ");
                 char weaponKey = Char.ToUpper(Console.ReadKey().KeyChar);
 
-                switch (weaponKey)
+                switch (key)
                 {
-                    case 'S':
-                        kpdn.Roll = LemparDadu(3);
-                        kpdn.Magic = (key == '1' || key == '3');
-                        kpdn.Flaming = (key == '2' || key == '3');
-                        Console.WriteLine(
-                           $"\nRolled {kpdn.Roll} for {kpdn.Damage} HP\n");
-
+                    case '0':
+                        takeSWitch('0', weaponKey, kpdn, kpnh);
                         break;
-
-                    case 'A':
-                        kpnh.Roll = LemparDadu(1);
-                        kpnh.Magic = (key == '1' || key == '3');
-                        kpnh.Flaming = (key == '2' || key == '3');
-                        Console.WriteLine(
-                           $"\nRolled {kpnh.Roll} for {kpnh.Damage} HP\n");
+                    case '1':
+                        takeSWitch('1', weaponKey, kpdn, kpnh);
                         break;
+                    case '2':
+                        takeSWitch('2', weaponKey, kpdn, kpnh);
+                        break;
+                    case '3':
+                        takeSWitch('3', weaponKey, kpdn, kpnh);
+                        break;
+                    default:
+                        return;
 
-                } //end switch
+                }
+                //switch (weaponKey)
+                //{
+                //    case 'S':
+                //        kpdn.Roll = LemparDadu(3);
+                //        kpdn.Magic = (key == '1' || key == '3');
+                //        kpdn.Flaming = (key == '2' || key == '3');
+                //        Console.WriteLine(
+                //           $"\nRolled {kpdn.Roll} for {kpdn.Damage} HP\n");
 
+                //        break;
 
-            }
+                //    case 'A':
+                //        kpnh.Roll = LemparDadu(1);
+                //        kpnh.Magic = (key == '1' || key == '3');
+                //        kpnh.Flaming = (key == '2' || key == '3');
+                //        Console.WriteLine(
+                //           $"\nRolled {kpnh.Roll} for {kpnh.Damage} HP\n");
+                //        break;
+
+                //} //end switch
+
+            } //END WHILE
+
+           
 
         } //end Main method
 
@@ -56,6 +76,31 @@ namespace Kerusakan_Part_02
             for (int i = 0; i < banyakLemparan; i++) total += acak.Next(1, 7);
             return total;
         } //end LemparDadu method
+
+
+        private static void takeSWitch (char key, char keyWeapon, SwordDamage kpdn, ArrowDamage kpnh)
+        {
+            switch (keyWeapon)
+            {
+                case 'S':
+                    kpdn.Roll = LemparDadu(3);
+                    kpdn.Magic = (key == '1' || key == '3');
+                    kpdn.Flaming = (key == '2' || key == '3');
+                    Console.WriteLine(
+                       $"\nRolled {kpdn.Roll} for {kpdn.Damage} HP\n");
+
+                    break;
+
+                case 'A':
+                    kpnh.Roll = LemparDadu(1);
+                    kpnh.Magic = (key == '1' || key == '3');
+                    kpnh.Flaming = (key == '2' || key == '3');
+                    Console.WriteLine(
+                       $"\nRolled {kpnh.Roll} for {kpnh.Damage} HP\n");
+                    break;
+
+            } //end switch
+        }
 
     } //end class
 } //end namespace
